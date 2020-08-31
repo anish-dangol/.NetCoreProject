dc17dbb5a83fe6d7530a872bdd8bc3e2d729447c
Anish Dangol <dangol.anish20@gmail.com>
2020-08-31 11:00:36 -0400

    first commit

---
+++ CarApi.Repositories/CarApiRepository.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CarApi.Context;
+using Microsoft.Extensions.DependencyInjection;
+using CarApi.Repositories.Interface;
+using CarApi.Models;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace CarApi.Repositories
+{
+    class CarApiRepository : ICarApiRepository
+
+    {
+        private readonly IServiceScope _scope;
+        private readonly CarApiDatabaseContext _databaseContext;
+
+        public CarApiRepository(IServiceProvider services)
+        {
+            _scope = services.CreateScope();
+
+            _databaseContext = _scope.ServiceProvider.GetRequiredService<CarApiDatabaseContext>();
+        }
+
+        public async Task<bool> Create(Vehicle vehicle)
+        {
+            var success = false;
+
+            _databaseContext.Vehicles.Add(vehicle);
+
+            var numberOfItemsCreated = await _databaseContext.SaveChangesAsync();
+
+            if (numberOfItemsCreated == 1)
+                success = true;
+
+            return success;
+
+        }
+
+        public async Task<bool> Update(Vehicle vehicle)
+        {
+            throw new NotImplementedException();
+
+        }
+
+        public Task<bool> Delete(string vehicleId)
+        {
+            throw new NotImplementedException();
+        }
+
+
+
+        //public async Task<ActionResult<Vehicle>> Get(int vehicleId)
+        //{
+        //    return await _context.Vehicles.ToListAsync();
+        //    ();
+        //}
+
+
+
+        public Vehicle Get(int vehicleId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ CarApi.Repositories/Interface/ICarApiRepository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using CarApi.Models;
+using System.Threading.Tasks;
+
+namespace CarApi.Repositories.Interface
+{
+   public interface ICarApiRepository
+    {
+        //Task<bool> Create(Vehicle vehicle);
+
+        //Task<bool> Update(Vehicle vehicle);
+
+        Vehicle Get(int vehicleId);
+
+        //IOrderedQueryable<BlogPost> GetAll();
+
+        //IOrderedQueryable<BlogPost> GetAllByUserAccountId(string userAccountId);
+
+         Task<bool> Delete(string vehicleId);
+
+    }
+}
